52a28292a22700c3dbb9f24e1f86167468fe6a1f
Andrzej Holub <aholubprof@gmail.com>
2018-09-09 17:30:27 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {render} from 'react-dom'
+
+
+function HelloWorld () {
+    return <h1>Hello world</h1>;
+}
+
+render(<HelloWorld />, document.getElementById('container'));
